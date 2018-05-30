5775e9f0d68fcc23467102ca0f8c13b903d8e470
smoke82 <38927066+smoke82@users.noreply.github.com>
2018-05-30 17:15:03 -0400

    Update consulta.php

--- consulta.php
+++ consulta.php
@@ -1,6 +1,13 @@
 <?php include_once 'session.php' ?>
 <script src="js/consultar.js"></script>
-
+<script src="jquery.rut.js"></script>
+<script>
+$(function() {
+    $("#rut").rut().on('rutValido', function(e, rut, dv) {
+        alert("El rut " + rut + "-" + dv + " es correcto");
+    }, { minimumLength: 7} );
+})
+</script>
 <section class="consulta">
     <b>Consulta</b>
     <p>
